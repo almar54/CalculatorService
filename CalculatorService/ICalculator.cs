b0c758f98bd94d9feead4c337046325ba0d848b2
almar54 <almarosette54@gmail.com>
2023-11-16 10:57:03 +0200

    Add project files.

---
+++ CalculatorService/ICalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace CalculatorService
+{
+    [ServiceContract]
+    public interface ICalculator
+    {
+        [OperationContract]
+        double Sum(double num1, double num2);
+        [OperationContract]
+        double Sub(double num1, double num2);
+        [OperationContract]
+        double Mul(double num1, double num2);
+        [OperationContract]
+        double Div(double num1, double num2);
+    }
+}
